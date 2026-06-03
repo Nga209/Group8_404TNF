00a7b738c6154357d84b7686fba367299abc8312
Nga209 <23070542@vnu.edu.vn>
2026-06-03 21:29:24 +0700

    3/6
    3/6

---
+++ config/config.php
@@ -0,0 +1,21 @@
+<?php
+date_default_timezone_set('Asia/Ho_Chi_Minh');
+
+define('DEVELOPMENT_MODE', true); // Đặt là true để kiểm thử ở localhost (Mock mode). Đặt thành false khi chạy thực tế (Production).
+
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', ''); 
+define('DB_NAME', 'campus_booking');
+
+// Thêm INS3064 vào đây
+define('BASE_URL', 'http://localhost/INS3064/campus_services_booking/backend/public');
+
+// Cấu hình gửi Mail (SMTP Gmail)
+define('SMTP_HOST', 'smtp.gmail.com');
+define('SMTP_PORT', 587);
+define('SMTP_USER', 'campusbook.test.vn@gmail.com'); // Gmail gửi thư
+define('SMTP_PASS', 'your_app_password_here');      // Mật khẩu ứng dụng Gmail (16 ký tự)
+define('SMTP_SECURE', 'tls');                      // Phương thức bảo mật: tls hoặc ssl
+define('SMTP_FROM', 'campusbook.test.vn@gmail.com');
+define('SMTP_FROM_NAME', 'CampusBook Service');
